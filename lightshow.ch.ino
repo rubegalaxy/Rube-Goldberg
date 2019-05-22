b0b3ad49b4bb0267b264e0687a8dd1de0515d136
rubegalaxy <50841408+rubegalaxy@users.noreply.github.com>
2019-05-22 10:46:30 -0700

    Add files via upload

---
+++ lightshow.ch.ino
@@ -0,0 +1,105 @@
+int ledpins[]={2,3,4,5,6,7,8,9};
+
+
+
+void setup() {
+  pinMode(ledpins[0],OUTPUT);
+  pinMode(ledpins[1],OUTPUT);
+  pinMode(ledpins[2],OUTPUT);
+  pinMode(ledpins[3],OUTPUT);
+  pinMode(ledpins[4],OUTPUT);
+  pinMode(ledpins[5],OUTPUT);
+  pinMode(ledpins[6],OUTPUT);
+  pinMode(ledpins[7],OUTPUT);
+  
+}
+
+void loop() {
+  digitalWrite(ledpins[7],HIGH);
+  delay(350);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[2],HIGH);
+  delay(350);
+  digitalWrite(ledpins[2],LOW);
+  digitalWrite(ledpins[0],HIGH);
+  delay(250);
+  digitalWrite(ledpins[0],LOW);
+  digitalWrite(ledpins[1],HIGH);
+  delay(300);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[0],HIGH);
+  digitalWrite(ledpins[1],HIGH);
+  digitalWrite(ledpins[6],HIGH);
+  digitalWrite(ledpins[7],HIGH);
+  delay(400);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[6],LOW);
+  digitalWrite(ledpins[0],LOW);
+  delay(275);
+  digitalWrite(ledpins[7],HIGH);
+  digitalWrite(ledpins[6],HIGH);
+  digitalWrite(ledpins[1],HIGH);
+  digitalWrite(ledpins[0],HIGH);
+  delay(275);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[6],LOW);
+  digitalWrite(ledpins[0],LOW);
+  delay(275);
+  digitalWrite(ledpins[7],HIGH);
+  digitalWrite(ledpins[6],HIGH);
+  digitalWrite(ledpins[1],HIGH);
+  digitalWrite(ledpins[0],HIGH);
+  delay(275);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[6],LOW);
+  digitalWrite(ledpins[0],LOW);
+  delay(275);
+  digitalWrite(ledpins[7],HIGH);
+  digitalWrite(ledpins[6],HIGH);
+  digitalWrite(ledpins[1],HIGH);
+  digitalWrite(ledpins[0],HIGH);
+  digitalWrite(ledpins[2],HIGH);
+  digitalWrite(ledpins[3],HIGH);
+  digitalWrite(ledpins[4],HIGH);
+  digitalWrite(ledpins[5],HIGH);
+  delay(275);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[6],LOW);
+  digitalWrite(ledpins[0],LOW);
+  digitalWrite(ledpins[3],HIGH);
+  digitalWrite(ledpins[4],HIGH);
+  digitalWrite(ledpins[5],HIGH);
+  digitalWrite(ledpins[2],HIGH);
+  delay(275);
+  digitalWrite(ledpins[7],HIGH);
+  digitalWrite(ledpins[6],HIGH);
+  digitalWrite(ledpins[1],HIGH);
+  digitalWrite(ledpins[0],HIGH);
+  digitalWrite(ledpins[2],HIGH);
+  digitalWrite(ledpins[3],HIGH);
+  digitalWrite(ledpins[4],HIGH);
+  digitalWrite(ledpins[5],HIGH);
+  delay(275);
+  digitalWrite(ledpins[7],LOW);
+  digitalWrite(ledpins[1],LOW);
+  digitalWrite(ledpins[6],LOW);
+  digitalWrite(ledpins[0],LOW);
+  digitalWrite(ledpins[3],HIGH);
+  digitalWrite(ledpins[4],HIGH);
+  digitalWrite(ledpins[5],HIGH);
+  digitalWrite(ledpins[2],HIGH);
+  delay(275);
+  
+  
+  
+  
+  
+  
+  
+  
+
+}
